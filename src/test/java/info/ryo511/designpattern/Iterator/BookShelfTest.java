17b43e37c9df088cf68367c626e291827c2d1dd6
Ryo Utsunomiya <millionsnowliving@gmail.com>
2020-08-26 22:56:20 +0900

    rename

--- src/test/java/info/ryo511/designpattern/Iterator/BookShelfTest.java
+++ src/test/java/info/ryo511/designpattern/Iterator/BookShelfTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-class BookShelfIteratorTest {
+class BookShelfTest {
 
   @Test
   void canIterateOverBookShelf() {
